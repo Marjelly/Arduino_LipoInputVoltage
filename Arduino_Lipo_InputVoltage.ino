12b0945e6189c0d56cd0787ab3d33b5358a52020
Marjelly <marjelly1@gmail.com>
2019-09-13 16:33:28 +0200

    Inital commit

---
+++ Arduino_Lipo_InputVoltage.ino
@@ -0,0 +1,43 @@
+int BatteryVoltageInput(A0);
+
+int  BatteryProzent[21]=(0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100);
+ 
+int BatteryVoltage[21]=();
+
+BatteryVoltage[1]==3.20;
+BatteryVoltage[2]==3.35;
+BatteryVoltage[1]==3.42;
+BatteryVoltage[1]==3.45;
+BatteryVoltage[1]==3.50; //20
+BatteryVoltage[1]==3.55;
+BatteryVoltage[1]==3.60; //30
+BatteryVoltage[1]==3.62;  
+BatteryVoltage[1]==3.64;
+BatteryVoltage[1]==3.66;
+BatteryVoltage[1]==3.68;
+BatteryVoltage[1]==3.70; //55
+BatteryVoltage[1]==3.72;
+BatteryVoltage[1]==3.75;
+BatteryVoltage[1]==3.77;
+BatteryVoltage[1]==3.80; //75
+BatteryVoltage[1]==3.83;
+BatteryVoltage[1]==3.86;
+BatteryVoltage[1]==3.90; //90
+BatteryVoltage[1]==4.00;
+BatteryVoltage[1]==4.20;
+ 
+void setup() {
+ pinMode(BatteryVoltageInput,INPUT);
+
+
+
+}
+
+void loop() {
+ 
+
+}
+void BatteryVoltageOutput(){
+  for(int x=1;x=>21,x++){
+    }
+  }
